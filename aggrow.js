86e5686ec277499fa887bdada9fc5ba096db928b
Darwish Quiambao <captain_200@hotmail.com>
2020-07-25 00:13:30 -0700

    ver-0.4-homePage
    First implementation of UI for weather from WeatherChannel Source codes

--- aggrow.js
+++ aggrow.js
@@ -7,9 +7,29 @@ function myFunction() {
     }
 }
 
-$(document).ready(function()) {
+//to prevent any jQuery code from running before the document is finished loading
+
+$(document).ready(function() {
 
     function weather() {
-
+        var URL = 'api.openweathermap.org/data/2.5/weather?q=Ukiah,CA,USA&appid=1ebcc78ddc0e6cd3f4400c4dfa13a255';
+        $.getJSON(URL, function(data) {
+            console.log(data);
+            updateDOM(data);
+        });
     }
-}
+
+    weather();
+
+    function updateDOM(data) {
+        var city = data.name;
+        var temp = data.main.temp;
+        /*        var icon = data.weather[0].icon;
+                var description = data.weather[0].description;*/
+        $('#city').html(city);
+        //$('#icon').html(icon);
+        $('#temp').html(temp);
+        //$('$description').html(description);
+    }
+
+});
